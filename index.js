8e2539763721ac6be25de4e4c8c2da54fa8d13b1
AliciaGaona <aligaovela@gmail.com>
2022-04-27 03:36:16 -0500

    III Creación de una clase y modularización

---
+++ index.js
@@ -0,0 +1,8 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const alicia = new MissionCommander("Alicia")
+const juana = new MissionCommander("Juana")
+
+console.log(carlo.name)
+console.log(alicia.name)
+console.log(juana.name)
