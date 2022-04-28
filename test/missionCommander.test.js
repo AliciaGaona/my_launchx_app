5c9935817043531febdb174105616f340cde5daa
AliciaGaona <aligaovela@gmail.com>
2022-04-28 01:10:39 -0500

    Se modifica archivo para fallar test

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,16 +1,16 @@
 const MissionCommander = require('./../app/missionCommander');
 
-describe("Esto es una suite de pruebas", () => {
-    test('Caso de prueba 1', () => {
-      const result = 1 + 2 
-      expect(result).toBe(3);
-    });
-  })
+// describe("Esto es una suite de pruebas", () => {
+//     test('Caso de prueba 1', () => {
+//       const result = 1 + 2 
+//       expect(result).toBe(3);
+//     });
+//   })
   
 
   describe("Unit Tests for Mission Commander Class", () => {
     test('1) Create a mission commander objet', () => {
-        const myMissionCommander = new MissionCommander("Alicia")
+        const myMissionCommander = new MissionCommander("j")
         expect(myMissionCommander.name).toBe("Alicia");
     });
   })
